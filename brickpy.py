135398c05e67d248edba605b9a016264b3e3af24
Andrew Nelson <skamphax0r@gmail.com>
2017-11-05 21:23:45 -0600

    sys args

--- brickpy.py
+++ brickpy.py
@@ -1,19 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
 
-data = {
-    'search_method': 'sku',
-    'sku': 'numbers',
-    'upc': '',
-    'zip': 'zip',
-    'sort': 'distance'
-}
 
-r = requests.post('https://brickseek.com/target-inventory-checker/', data=data)
+def target(data):
+    ''' Check target for availability '''
+    data = data
+    r = requests.post('https://brickseek.com/target-inventory-checker/',
+                      data=data)
 
-soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-for avail in soup.find_all(class_='availability-2'):
-    avail.parent.parent.parent.find('h4').text
-    avail.parent.parent.parent.find(class_='store-address').text
-    avail.parent.parent.parent.find(class_='store-price').text
+    for avail in soup.find_all(class_='availability-2'):
+        print('store-name: '+avail.parent.parent.parent.find('h4').text)
+        print('address: ' +
+              avail.parent.parent.parent.find(class_='store-address').text)
+        print('price (if available) :' +
+              avail.parent.parent.parent.find(class_='store-price').text.strip()) # NOQA
+        print('---------')
+
+
+def main(**kwargs):
+    sku = kwargs['sku']
+    zipcode = kwargs['zip']
+    data = {
+        'search_method': 'sku',
+        'sku': sku,
+        'upc': '',
+        'zip': zipcode,
+        'sort': 'distance'
+    }
+    target(data)
+
+
+main(sku=sys.argv[1], zip=sys.argv[2])
