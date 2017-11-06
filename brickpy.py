93b06e35f08eb112155ccf616a90545b2b839847
Andrew Nelson <skamphax0r@gmail.com>
2017-11-05 21:42:23 -0600

    cleanup

--- brickpy.py
+++ brickpy.py
@@ -1,26 +1,29 @@
-from bs4 import BeautifulSoup
-import requests
+''' brickpy module. '''
 import sys
+import requests
+from bs4 import BeautifulSoup
 
 
 def target(data):
-    ''' Check target for availability '''
+    ''' Check target for availability. '''
     data = data
-    r = requests.post('https://brickseek.com/target-inventory-checker/',
-                      data=data)
+    lookup = requests.post('https://brickseek.com/target-inventory-checker/',
+                           data=data)
 
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(lookup.text, 'html.parser')
 
     for avail in soup.find_all(class_='availability-2'):
         print('store-name: '+avail.parent.parent.parent.find('h4').text)
         print('address: ' +
               avail.parent.parent.parent.find(class_='store-address').text)
         print('price (if available) :' +
-              avail.parent.parent.parent.find(class_='store-price').text.strip()) # NOQA
+              avail.parent.parent.parent.find(class_='store-price').text
+              .strip())
         print('---------')
 
 
 def main(**kwargs):
+    ''' main function. '''
     sku = kwargs['sku']
     zipcode = kwargs['zip']
     data = {
@@ -33,4 +36,5 @@ def main(**kwargs):
     target(data)
 
 
-main(sku=sys.argv[1], zip=sys.argv[2])
+if __name__ == '__main__':
+    main(sku=sys.argv[1], zip=sys.argv[2])
